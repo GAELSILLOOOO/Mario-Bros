f26ce895952919beb53535f652956be6b6113c92
GAELSILLOOOO <A23310295@live.ceti.mx>
2024-10-15 00:21:45 +0000

    Clase 1

---
+++ include/Porcentaje.hpp
@@ -0,0 +1,30 @@
+#include <stdexcept>
+class Porcentaje
+{
+private:
+    int numero;
+
+public:
+    Porcentaje()
+    {
+        this->numero = 0;
+    }
+    Porcentaje(int numero)
+    {
+        if (numero > 100 || numero < 0)
+        {
+            throw new std ::runtime_error("NUMERO FUERA DE RANGO")
+        }
+        else
+        {
+            this->numero = numero;
+        }
+    }
+    ~Porcentaje() {}
+    void Incrementar(int numero)
+    {
+    }
+    void Disminuir(int numero)
+    {
+    }
+};
