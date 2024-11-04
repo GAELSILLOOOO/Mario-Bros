cde87fc2cd234bd49b03d305efb77fcc04a44abf
GAELSILLOOOO <A23310295@live.ceti.mx>
2024-11-04 23:27:54 +0000

    EXAMENNN

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -37,16 +37,18 @@ int main(int argc, char const *argv[])
             }
 
             // Dibujo de las ruedas del carro Formula 1
-            can.DrawPointCircle(rueda1_x, 40, 5); // Rueda 1
-            can.DrawPointCircle(rueda2_x, 40, 5); // Rueda 2
+            can.DrawPointCircle(rueda1_x, 40, 5, Color::GrayLight); // Rueda 1
+            can.DrawPointCircle(rueda2_x, 40, 5, Color::GrayLight); // Rueda 2
 
             // Dibujo del cuerpo del carro
-            can.DrawPointLine(15 + frame, 35, 52 + frame, 35); // Línea base de la carrocería
-            can.DrawPointLine(15 + frame, 22, 45 + frame, 22); // Techo del carro
-            can.DrawPointLine(15 + frame, 35, 15 + frame, 22); // Línea delantera
-            can.DrawPointLine(45 + frame, 22, 45 + frame, 27); // línea trasera
-            can.DrawPointLine(45 + frame, 27, 52 + frame, 27); // líneas frontales
-            can.DrawPointLine(52 + frame, 35, 52 + frame, 28); // líneas frontales
+            can.DrawPointLine(15 + frame, 35, 52 + frame, 35, Color::OrangeRed1); // Línea base de la carrocería
+            can.DrawPointLine(15 + frame, 22, 45 + frame, 22, Color::OrangeRed1); // Techo del carro
+            can.DrawPointLine(15 + frame, 35, 15 + frame, 22, Color::OrangeRed1); // Línea delantera
+            can.DrawPointLine(45 + frame, 22, 45 + frame, 27, Color::OrangeRed1); // línea trasera
+            can.DrawPointLine(45 + frame, 27, 52 + frame, 27, Color::OrangeRed1); // líneas frontales
+            can.DrawPointLine(52 + frame, 35, 52 + frame, 28, Color::OrangeRed1); // líneas frontales
+            can.DrawPointLine(1, 48, 200, 48, Color::Green1);                     // lineas pista
+            can.DrawPointLine(1, 10, 200, 10, Color::Green1);                     // lineas pista
 
             Element lienzo = bgcolor(Color::BlueViolet, border(vbox(ColorSupport, canvas(&can))));
             Render(pantalla, lienzo);
